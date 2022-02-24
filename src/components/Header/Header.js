0d4de81001b7ed2fbe3d43d4dac6251b33547947
Ricky Tanudjaja CT <ricky.tanudjaja@cartrack.com>
2022-02-24 20:55:47 +0700

    exercise2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS, QUERIES } from '../../constants';
+import { COLORS, QUERIES, WEIGHTS } from '../../constants';
 import Logo from '../Logo';
 import Icon from '../Icon';
+import UnstyledButton from '../UnstyledButton';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import VisuallyHidden from '../VisuallyHidden';
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -19,29 +21,33 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Side>
+        <LogoWrapper>
           <Logo />
-        </Side>
-        <Nav>
+        </LogoWrapper>
+        <DesktopNav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
           <NavLink href="/men">Men</NavLink>
           <NavLink href="/women">Women</NavLink>
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        </DesktopNav>
+        <MobileActions>
+          <ShoppingBagButton>
+            <Icon id="shopping-bag" />
+            <VisuallyHidden>Open cart</VisuallyHidden>
+          </ShoppingBagButton>
+          <UnstyledButton>
+            <Icon id="search" />
+            <VisuallyHidden>Search</VisuallyHidden>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id="menu" />
+            <VisuallyHidden>Open menu</VisuallyHidden>
+          </UnstyledButton>
+        </MobileActions>
+        <Filler />
       </MainHeader>
-      <AlternateHeader>
-        <Side>
-          <Logo />
-        </Side>
-        <Nav>
-          <Icon id="shopping-bag" strokeWidth={1} />
-          <Icon id="search" strokeWidth={1} />
-          <Icon id="menu" strokeWidth={1} />
-        </Nav>
-      </AlternateHeader>
 
       <MobileMenu
         isOpen={showMobileMenu}
@@ -50,51 +56,60 @@ const Header = () => {
     </header>
   );
 };
- 
+
 const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  @media ${QUERIES.tabletAndSmaller} {
+    justify-content: space-between;
+    align-items: center;
+    border-top: 4px solid ${COLORS.gray[900]};
+  }
+  @media ${QUERIES.phoneAndSmaller} {
+    padding-left: 16px;
+    padding-right: 16px;
+  }
+`;
 
-  @media ${QUERIES.laptopAndDown} {
+const DesktopNav = styled.nav`
+  display: flex;
+  gap: 48px;
+  margin: 0px 48px;
+  @media ${QUERIES.tabletAndSmaller} {
     display: none;
   }
 `;
 
-const AlternateHeader = styled.div`
+const MobileActions = styled.div`
   display: none;
-  align-items: baseline;
-  justify-content: flex-end;
-  padding: 18px 32px;
-  height: 72px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
-
-  @media ${QUERIES.laptopAndDown} {
+  @media ${QUERIES.tabletAndSmaller} {
+    gap: 32px;
     display: flex;
+  }
+  @media ${QUERIES.phoneAndSmaller} {
+    gap: 16px;
   }
 `;
 
-const Nav = styled.nav`
-  display: flex;
-  gap: 48px;
-  margin: 0px 48px;
-  @media ${QUERIES.laptopAndDown} {
-    margin: 0px;
-    display: flex;
-    gap: 48px;
-  }
-  @media ${QUERIES.tabletAndDown} {
-    gap: 34px;
-  }
-  @media ${QUERIES.mobileAndDown} {
-    gap: 20px
-  }
-`;
-
-const Side = styled.div`
+const LogoWrapper = styled.div`
   flex: 1;
+  @media ${QUERIES.tabletAndSmaller} {
+    flex: revert;
+  }
+`;
+
+const ShoppingBagButton = styled(UnstyledButton)`
+  transform: translateX(-2px);
+`;
+
+const Filler = styled.div`
+  flex: 1;
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
 `;
 
 const NavLink = styled.a`
@@ -103,7 +118,6 @@ const NavLink = styled.a`
   text-decoration: none;
   color: ${COLORS.gray[900]};
   font-weight: ${WEIGHTS.medium};
-
   &:first-of-type {
     color: ${COLORS.secondary};
   }
